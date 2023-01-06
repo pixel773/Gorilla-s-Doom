df896a7310f1e25fb8f50ac6c5cc7d71e6026a42
MrBanana <72209985+pixel773@users.noreply.github.com>
2023-01-06 14:27:23 -0600

    Update Plugin.cs

--- Gorilla'sDoom/Plugin.cs
+++ Gorilla'sDoom/Plugin.cs
@@ -1,4 +1,4 @@
-﻿using BepInEx;
+using BepInEx;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +6,13 @@ using Utilla;
 using GorillasDoom.Scripts;
 using GorillaNetworking;
 
-namespace GorillasDoom
+namespace Gorilla_sDoom
 {
+    /// <summary>
+    /// This is your mod's main class.
+    /// </summary>
+
+    /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
     [ModdedGamemode]
     [BepInDependency("org.legoandmars.gorillatag.utilla", "1.6.7")]
     [ModdedGamemode("gorillasdoom", "DOOM", Utilla.Models.BaseGamemode.Infection)]
@@ -21,7 +26,7 @@ namespace GorillasDoom
         // Timers
         private float timeUntilNextTimer = 5f;
         private float timeUntilDecreaseCountdownTimer = 1;
-        
+
         // Events
         private int currentEvent = 0;
         private int currentCountdown;
@@ -30,7 +35,26 @@ namespace GorillasDoom
         private GameObject trees;
         private GameObject floor;
         private GameObject walls;
+        private GameObject campgroundstructure;
+        private GameObject SoundPostForest;
+        private GameObject campfire;
+        private GameObject slide;
+        private GameObject christmastree_prefab;
+        private GameObject newTreehouse;
+
         private bool inForest;
+
+        //I wasted 15 mins doing this :O
+        private GameObject flatpanel5;
+        private GameObject flatpanel9;
+        private GameObject flatpanel10;
+        private GameObject flatpanel11;
+        private GameObject flatpanel12;
+        private GameObject flatpanel13;
+        private GameObject flatpanel14;
+        private GameObject flatpanel15;
+        private GameObject flatpanel16;
+        private GameObject flatpanel17;
 
         private readonly List<string> eventText = new List<string>()
         {
@@ -47,7 +71,9 @@ namespace GorillasDoom
             "visible walls",
             "small scale",
             "normal scale",
-            "large scale"
+            "large scale",
+            "hidden forest objects",
+            "visible forest objects"
         };
 
         public Dictionary<string, bool> isGood = new Dictionary<string, bool>()
@@ -65,7 +91,9 @@ namespace GorillasDoom
             {"visible walls", true },
             {"small scale", false },
             {"normal scale", true },
-            {"large scale", false }
+            {"large scale", true },
+            {"hidden forest objects", false },
+            {"visible forest objects", true }
         };
 
         internal void Start()
@@ -74,16 +102,41 @@ namespace GorillasDoom
             Events.GameInitialized += OnGameInitialized;
         }
 
+        void OnDisable()
+        {
+            HarmonyPatches.RemoveHarmonyPatches();
+            Manager.ResetGame();
+        }
+
         private void OnGameInitialized(object sender, EventArgs e)
         {
             floor = GameObject.Find("pit ground");
             walls = GameObject.Find("pit wall");
             trees = GameObject.Find("SmallTrees");
+            campgroundstructure = GameObject.Find("campgroundstructure");
+            SoundPostForest = GameObject.Find("SoundPostForest");
+            campfire = GameObject.Find("campfire");
+            slide = GameObject.Find("slide");
+            christmastree_prefab = GameObject.Find("christmastree_prefab");
+            newTreehouse = GameObject.Find("newTreehouse (1)");
+
+
+            //I wasted 15 mins doing this :O
+            flatpanel5 = GameObject.Find("flatpanel(5)");
+            flatpanel9 = GameObject.Find("flatpanel(9)");
+            flatpanel10 = GameObject.Find("flatpanel(10)");
+            flatpanel11 = GameObject.Find("flatpanel(11)");
+            flatpanel12 = GameObject.Find("flatpanel(12)");
+            flatpanel13 = GameObject.Find("flatpanel(13)");
+            flatpanel14 = GameObject.Find("flatpanel(14)");
+            flatpanel15 = GameObject.Find("flatpanel(15)");
+            flatpanel16 = GameObject.Find("flatpanel(16)");
+            flatpanel17 = GameObject.Find("flatpanel(17)");
         }
 
         internal void Update()
         {
-            if(InRoom && inForest) // Make sure we're both in a room and in the forest map before handling timers
+            if (InRoom && inForest) // Make sure we're both in a room and in the forest map before handling timers
             {
                 timeUntilNextTimer -= Time.deltaTime;
 
@@ -141,7 +194,7 @@ namespace GorillasDoom
                     Manager.FlingPlayer(new System.Random().Next(0, 2) == 0 ? 2 : -2);
                     break;
                 case 7:
-                    Manager.EnhanceSpeed(3, 1.75f);
+                    Manager.EnhanceSpeed(8, 1.5f);
                     break;
                 case 8:
                     Manager.SetItem(trees, true);
@@ -153,13 +206,51 @@ namespace GorillasDoom
                     Manager.SetItem(walls, true);
                     break;
                 case 11:
-                    Manager.SetScale(0.1f, true);
+                    Manager.SetScale(0.5f, true);
                     break;
                 case 12:
                     Manager.SetScale(1, false);
                     break;
                 case 13:
                     Manager.SetScale(2, true);
+                    break;
+                case 14:
+                    Manager.SetItem(slide, false);
+                    Manager.SetItem(campfire, false);
+                    Manager.SetItem(campgroundstructure, false);
+                    Manager.SetItem(SoundPostForest, false);
+                    Manager.SetItem(christmastree_prefab, false);
+                    Manager.SetItem(newTreehouse, false);
+                    Manager.SetItem(flatpanel5, false);
+                    Manager.SetItem(flatpanel9, false);
+                    Manager.SetItem(flatpanel10, false);
+                    Manager.SetItem(flatpanel11, false);
+                    Manager.SetItem(flatpanel12, false);
+                    Manager.SetItem(flatpanel13, false);
+                    Manager.SetItem(flatpanel14, false);
+                    Manager.SetItem(flatpanel15, false);
+                    Manager.SetItem(flatpanel16, false);
+                    Manager.SetItem(flatpanel17, false);
+                    break;
+
+                case 15:
+                    Manager.SetItem(slide, true);
+                    Manager.SetItem(campfire, true);
+                    Manager.SetItem(campgroundstructure, true);
+                    Manager.SetItem(SoundPostForest, true);
+                    Manager.SetItem(christmastree_prefab, true);
+                    Manager.SetItem(newTreehouse, true);
+
+                    Manager.SetItem(flatpanel5, true);
+                    Manager.SetItem(flatpanel9, true);
+                    Manager.SetItem(flatpanel10, true);
+                    Manager.SetItem(flatpanel11, true);
+                    Manager.SetItem(flatpanel12, true);
+                    Manager.SetItem(flatpanel13, true);
+                    Manager.SetItem(flatpanel14, true);
+                    Manager.SetItem(flatpanel15, true);
+                    Manager.SetItem(flatpanel16, true);
+                    Manager.SetItem(flatpanel17, true);
                     break;
             }
         }
@@ -177,7 +268,8 @@ namespace GorillasDoom
         #endregion
         #region Modded room methods
 
-        [ModdedGamemodeJoin] internal void OnJoin(string gamemode)
+        [ModdedGamemodeJoin]
+        internal void OnJoin(string gamemode)
         {
             if ((gamemode.Contains("gorillasdoom") || gamemode.Contains("gorillasdoomcasual")) && Photon.Pun.PhotonNetwork.CurrentRoom.IsVisible)
             {
@@ -191,7 +283,8 @@ namespace GorillasDoom
             }
         }
 
-        [ModdedGamemodeLeave] internal void OnLeave(string gamemode)
+        [ModdedGamemodeLeave]
+        internal void OnLeave(string gamemode)
         {
             InRoom = false;
             Manager.ResetGame();
